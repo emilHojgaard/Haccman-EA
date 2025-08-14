f7a3b8fee542af01c6b8986c4fec7907dd473723
emilHojgaard <emilhn94@gmail.com>
2025-08-14 12:21:00 +0200

    deleted dead code

--- frontend/src/pages/PromptPage.jsx
+++ frontend/src/pages/PromptPage.jsx
@@ -594,34 +594,6 @@ function Prompt(props) {
                 list_of_challenges={list_of_challenges}
                 currentChallenge={currentChallenge}
               />
-              {/* <div
-                id="llm-box"
-                style={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  flexDirection: "column",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    border: "2px solid #000000",
-                    background: "#000000",
-                    color: "#FFFADE",
-                    padding: "10px",
-                    fontSize: "35px",
-                    fontFamily: "ARCADE_I",
-                    textShadow: "4px 4px 0px #A9345C",
-                  }}
-                >
-                  {">>>JAILBREAK CHALLENGE: "}
-                </div>
-                <div style={{ color: "#FFFADE" }}>
-                  {list_of_challenges[
-                    currentChallenge
-                  ].description.toUpperCase()}
-                </div>
-              </div> */}
 
               {/* Refactored */}
               <div
