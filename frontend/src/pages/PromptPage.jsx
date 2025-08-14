7c65defcc81c2624b4cd6b331244ada8ebe634e2
emilHojgaard <emilhn94@gmail.com>
2025-08-14 14:04:54 +0200

    Refactored WinScreen

--- frontend/src/pages/PromptPage.jsx
+++ frontend/src/pages/PromptPage.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import AssetInfo from "../AssetInfo";
 import { useSoundEffect } from "../SoundEffectContext";
 import BaseUI from "./promptPage-components/BaseUI";
+import WinScreen from "./promptPage-components/WinScreen";
 
 function Prompt(props) {
   // Initialize state for the input value
@@ -314,94 +315,11 @@ function Prompt(props) {
   return (
     <>
       {showContent && (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div className="beaten">
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "end",
-                  border: "2px solid #FFFADE",
-                  background: "#000000",
-                }}
-              >
-                <button
-                  onClick={() => {
-                    setInitialInfo(false);
-                  }}
-                >
-                  x
-                </button>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  border: "2px solid #FFFADE",
-                  background: "#000000",
-                  height: "400px",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    background: "#000000",
-                    gap: "10px",
-                    overflowY: "scroll",
-                    letterSpacing: "1px",
-                    lineHeight: "30px",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src="/Winner trophy 1.png"
-                    alt="Custom Image"
-                    class="win"
-                  ></img>
-                  <div
-                    style={{
-                      padding: "10px",
-                      fontFamily: "ARCADE_I",
-                      color: "#FFFADE",
-                      textShadow: "4px 4px 0px #A9345C",
-                      fontSize: 40,
-                    }}
-                  >
-                    {">>>>JAILBROKEN<<<<"}
-                  </div>
-
-                  <div style={{ padding: "10px", color: "#ffffff" }}>
-                    {"You broke through the LLM guardrails!"}
-                  </div>
-                </div>
-              </div>
-              <button
-                onClick={() => {
-                  setWinState(false);
-                  setShowContent(false);
-                }}
-              >
-                {" "}
-                Press Esc to hacc on!{" "}
-              </button>
-            </div>
-          </div>
-          <div className="beaten-back"></div>
-        </div>
+        <WinScreen
+          setInitialInfo={setInitialInfo}
+          setWinState={setWinState}
+          setShowContent={setShowContent}
+        />
       )}
 
       {specificOn && guardarailInfo && (
---
+++ frontend/src/pages/promptPage-components/WinScreen.jsx
@@ -0,0 +1,96 @@
+export default function WinScreen({
+  setInitialInfo,
+  setWinState,
+  setShowContent,
+}) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div className="beaten">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              border: "2px solid #FFFADE",
+              background: "#000000",
+            }}
+          >
+            <button
+              onClick={() => {
+                setInitialInfo(false);
+              }}
+            >
+              x
+            </button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              border: "2px solid #FFFADE",
+              background: "#000000",
+              height: "400px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                background: "#000000",
+                gap: "10px",
+                overflowY: "scroll",
+                letterSpacing: "1px",
+                lineHeight: "30px",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="/Winner trophy 1.png"
+                alt="Custom Image"
+                class="win"
+              ></img>
+              <div
+                style={{
+                  padding: "10px",
+                  fontFamily: "ARCADE_I",
+                  color: "#FFFADE",
+                  textShadow: "4px 4px 0px #A9345C",
+                  fontSize: 40,
+                }}
+              >
+                {">>>>JAILBROKEN<<<<"}
+              </div>
+
+              <div style={{ padding: "10px", color: "#ffffff" }}>
+                {"You broke through the LLM guardrails!"}
+              </div>
+            </div>
+          </div>
+          <button
+            onClick={() => {
+              setWinState(false);
+              setShowContent(false);
+            }}
+          >
+            {" "}
+            Press Esc to hacc on!{" "}
+          </button>
+        </div>
+      </div>
+      <div className="beaten-back"></div>
+    </div>
+  );
+}
