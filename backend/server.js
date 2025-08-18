a075ce2aeb004dd2f5811f067e2524e3002620f4
emilHojgaard <emilhn94@gmail.com>
2025-08-18 15:19:29 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const apiRoutes = require('./apiRoutes');
-const cors = require('cors')
-//const someMiddleware = require('./middleware/someMiddleware');
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import apiRoutes from './apiRoutes.js';
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000
-
-app.use(cors())
+// Middleware
+app.use(cors());
 app.use(express.json());
-app.use('/api8989', apiRoutes)
 
+// Routes
+app.use('/api8989', apiRoutes);
 
-/* app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}); */
-
-app.listen(process.env.PORT || 3000)
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
