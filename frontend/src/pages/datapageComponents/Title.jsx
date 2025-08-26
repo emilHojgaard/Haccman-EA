ea851f883ceb70988e02d97bef4c09ea84991f6c
AnyChristensen <anne.ny.christensen@gmail.com>
2025-08-26 12:24:14 +0200

    tailwind config

--- frontend/src/pages/datapageComponents/Title.jsx
+++ frontend/src/pages/datapageComponents/Title.jsx
@@ -4,37 +4,36 @@ export default function Title() {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "relative",
-        background: "linear-gradient(to right, #ff7e5f, #feb47b)",
-        padding: "12px 0",
-      }}
-    >
-      <button
-        className="vaporwave-input"
-        onClick={() => navigate(-1)}
-        style={{
-          position: "absolute",
-          left: "16px",
-          border: "none",
-          background: "none",
-          cursor: "pointer",
-          boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-        }}
-      >
-        Go Back
-      </button>
+    // <div
+    //   className="flex flex-row justify-center bg-black "
+    //   // style={{
+    //   //   display: "flex",
+    //   //   alignItems: "center",
+    //   //   justifyContent: "center",
+    //   //   position: "relative",
+    //   //   background: "linear-gradient(to right, #ff7e5f, #feb47b)",
+    //   //   padding: "12px 0",
+    //   // }}
+    // >
+    //   <button
+    //     className="vaporwave-input"
+    //     onClick={() => navigate(-1)}
+    //     style={{
+    //       position: "absolute",
+    //       left: "16px",
+    //       border: "none",
+    //       background: "none",
+    //       cursor: "pointer",
+    //       boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+    //     }}
+    //   >
+    //     Go Back
+    //   </button>
+    //   <h1 className="text-[#FFFADE] font-arcade">Data Page</h1>
+    // </div>
 
-      <h1
-        className="vaporwave-input"
-        style={{ fontSize: "30px", margin: 0, border: "none" }}
-      >
-        Data Page
-      </h1>
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-4xl">
+      Tailwind virker!
     </div>
   );
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { fontFamily: { arcade: ["ARCADE_I", "sans-serif"] } } },
+  plugins: [],
+};
